c62d845ce920f3aae84c8a6e549c39f35b3d7a62
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-09-13 14:27:14 -0300

    update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,11 @@
 import express from 'express';
 
 const PORT = 3000;
+
 const app = express();
 
-const obj = {
-  name: 'Marcus',
-  age: 19,
-  occupation: 'dev'
-}
-
 app.get('/', (req, res) => {
-  res.json({oi:'Hello World!', obj});
+  res.send('Hello World!');
 })
 
 app.listen(PORT, () => console.log(`Running at ${PORT}...`))
